e393e3b4e9b72b0676644ea51e8c3e556c205c98
sudhakar-1 <65119823+sudhakar-1@users.noreply.github.com>
2020-06-16 10:04:08 -0700

    Add files via upload

---
+++ p.js
@@ -0,0 +1,16 @@
+{
+  "success": true,
+  "type": "rich",
+  "version": "1.0",
+  "provider_name": "CodePen",
+  "provider_url": "https://codepen.io",
+  "title": "Credit Card Payment Form",
+  "author_name": "Ammon Rees",
+  "author_url": "https://codepen.io/ammonious/",
+  "height": "300",
+  "width": "800",
+  "thumbnail_width": "384",
+  "thumbnail_height": "225",
+  "thumbnail_url": "https://screenshot.codepen.io/2428377.ZqGeqX.small.fc85dac5-2568-49fa-915d-0f8d240bd2f9.png",
+  "html": "<iframe id=\"cp_embed_ZqGeqX\" src=\"https://codepen.io/ammonious/embed/preview/ZqGeqX?height=300&amp;slug-hash=ZqGeqX&amp;default-tabs=js,result&amp;host=https://codepen.io\" title=\"Credit Card Payment Form\" scrolling=\"no\" frameborder=\"0\" height=\"300\" allowtransparency=\"true\" class=\"cp_embed_iframe\" style=\"width: 100%; overflow: hidden;\"></iframe>"
+}
